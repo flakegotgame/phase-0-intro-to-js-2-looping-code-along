34dfe99cb1300c6d41f5b3d2857b2c7097a578e3
flakegotgame <victorchange808@gmail.com>
2024-03-22 11:19:24 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names) {
+    const messages = [];
+    for (const name of names) {
+      messages.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+    }
+    return messages;
+  }
+
+function countDown(number) {
+
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+
+  countDown(10);
+
